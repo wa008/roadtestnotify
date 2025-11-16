034463142a04f878fd18f91926528682c53f4e6e
zphu1024 <zphu1024@gmail.com>
2025-11-16 11:58:57 -0500

    add restart

--- statistics/restart_statistics.sh
+++ statistics/restart_statistics.sh
@@ -1,11 +1,11 @@
 # check whether OS is macos or not, if os is macos PYTHON_HOME="/opt/homebrew/opt/python@3.11/bin/python3.11", else PYTHON_HOME="/opt/homebrew/opt/python@3.11/bin/python3.11"
-# if [ "$(uname)" == "Darwin" ]; then
-#     PYTHON_HOME="/opt/homebrew/opt/python@3.11/bin/python3.11"
-# else
-#     PYTHON_HOME="/home/azureuser/.local/share/pypoetry/venv/bin/python"
-# fi
+if [ "$(uname)" == "Darwin" ]; then
+    PYTHON_HOME="/opt/homebrew/opt/python@3.11/bin/python3.11"
+else
+    PYTHON_HOME="/home/azureuser/.local/share/pypoetry/venv/bin/python"
+fi
 
-# ${PYTHON_HOME} -u statistics_logs.py
+${PYTHON_HOME} -u statistics_logs.py
 
 # git check whether statistics_logs_by_hour.txt is changed, if changed git add and git commit -m "update statistics_logs_by_hour.txt" and git push
 
